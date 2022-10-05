9d67489f288d04ba92317b8e1c7b1f963b1d489b
lorenz aichner <aichnerl@yahoo.de>
2022-10-05 15:40:35 +0200

    finally works something

--- cdt/causal_discovery.py
+++ cdt/causal_discovery.py
@@ -6,22 +6,20 @@ import cdt.independence.graph
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from cdt.causality.pairwise import ANM
-from cdt.causality.pairwise import BivariateFit
-from cdt.causality.pairwise import CDS
-from cdt.causality import graph
-from cdt.data import AcyclicGraphGenerator
 
+from sklearn.datasets import load_boston
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
+from cdt.independence.graph import ARD
 from cdt.data import load_dataset
 
 
 # Use a breakpoint in the code line below to debug your script.
 
-def CausalDiscovery(path, cd_algorithm, graph_recovery):
-    CDAlgorithm = getCDAlgortihm(cd_algorithm)
-    data, graph = load_data_and_skeletton(path, graph_recovery)
+def CausalDiscovery(path,causal_discovery,graph_recovery,skeleton, directed_sceleton):
+    CDAlgorithm = getPairwiseCausalityAlgorithm(causal_discovery)
+    data, graph = load_data_and_skeletton(path, graph_recovery, skeleton, directed_sceleton)
     output = CDAlgorithm.orient_graph(data, nx.DiGraph(graph))
     nx.draw_networkx(output, font_size=8)
     plt.show()
@@ -35,25 +33,41 @@ def CausalDiscoveryJustPredict(path, cd_algorithm):
     nx.draw_networkx(output, font_size=8)
     plt.show()
 
-def getCDAlgortihm(cd_algorithm):
+def getPairwiseCausalityAlgorithm(cd_algorithm):
     if(cd_algorithm == "ANM"):
         return cdt.causality.pairwise.ANM
     return "error"
 
-def load_data_and_skeletton(path, graph_recovery):
+def load_data_and_skeletton(path, graph_recovery, skeleton, directed_skeleton):
     data_ = pd.read_csv(path, sep=',')
-    if(graph_recovery == "basic"):
-        skeleton = cdt.causality.graph.bnlearn.BNlearnAlgorithm.create_graph_from_data(data= data_)
-        return (data, skeleton)
-    recoery_algorithm = getSkeletonRecoveryAlgorithm(graph_recovery)
-    skeleton = recoery_algorithm.predict(pd.DataFrame(data_))
+    #boston = load_boston()
+    #data_ = pd.DataFrame(boston['data'])
+    graph: list
+    if(skeleton):
+        graph = recoverSkeletton(data_, directed_skeleton)
+    print(graph)
+    print("Graph Created")
+    recoery_algorithm = getGraphCausalityAlgorithm(graph_recovery)
+    output = recoery_algorithm.predict(data_,graph)
+    nx.draw_networkx(output, font_size=8)
+    plt.show()
     return (data_, skeleton)
 
-def getSkeletonRecoveryAlgorithm(graph_recovery):
+def recoverSkeletton(data,directedSceleton):
+    if(directedSceleton):
+        return ""
+    else:
+        graph = ARD().predict(data)
+        return graph
+
+
+def getGraphCausalityAlgorithm(graph_recovery):
     if(graph_recovery == "FSGNN"):
         return cdt.independence.graph.FSGNN()
     elif(graph_recovery == "Glasso"):
         return cdt.independence.graph.Glasso()
+    elif(graph_recovery == "CGNN"):
+        return cdt.causality.graph.CGNN()
     return "Error"
 
 def ANMAlgoTheirData():
@@ -73,7 +87,11 @@ def ANMAlgoTheirData():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     path = "/home/lorenz/Documents/Bachelor/master-project/Datasets/sachs.csv"
-    graph_recovery = "basic"
+    graph_recovery = "CGNN"
     causal_discovery = "ANM"
-    CausalDiscovery(path,causal_discovery,graph_recovery)
+
+    skeleton = 1
+    directed_sceleton = 0
+    CausalDiscovery(path,causal_discovery,graph_recovery,skeleton, directed_sceleton)
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
